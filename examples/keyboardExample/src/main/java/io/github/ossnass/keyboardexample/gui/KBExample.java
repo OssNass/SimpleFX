6d92a1f9bda3896984ab71d2159bd73e42c731a8
Ossama Nasser <ossnass@gmail.com>
2020-05-26 09:04:49 +0300

    Update KBExample.java

--- examples/keyboardExample/src/main/java/io/github/ossnass/keyboardexample/gui/KBExample.java
+++ examples/keyboardExample/src/main/java/io/github/ossnass/keyboardexample/gui/KBExample.java
@@ -2,12 +2,43 @@ package io.github.ossnass.keyboardexample.gui;
 
 import io.github.ossnass.fx.ContollerType;
 import io.github.ossnass.fx.ControllerInfo;
+import io.github.ossnass.fx.QuickActions;
 import io.github.ossnass.fx.SimpleController;
+import io.github.ossnass.fx.keyboard.KBShortcut;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCombination;
 
 @ControllerInfo(Id = "KBEMain",FXMLFile = "/fxmls/kbmain.fxml",Type = ContollerType.SINGLE_INSTANCE_ON_STARTUP)
 public class KBExample extends SimpleController {
+    @FXML
+    private Button btnA;
+
+    @FXML
+    private Button btnB;
+
+    @FXML
+    private Button btnC;
+
+    @FXML
+    void btnAClick(ActionEvent event) {
+        QuickActions.showInfoMessage(null,"Info Message","Info body",this);
+    }
+
+    @FXML
+    void btnBClick(ActionEvent event) {
+        QuickActions.showWarningMessage(null,"Warning Message","Warning body",this);
+    }
+
+    @FXML
+    void btnCClick(ActionEvent event) {
+        QuickActions.showErrorMessage(null,"Error Message","Error body",this);
+    }
     @Override
     protected void userInit() {
-        
+        kbsm.addKeyboardShortcut("Info",new KBShortcut<ActionEvent>(btnA, KeyCombination.valueOf("ctrl+a"),this::btnAClick));
+        kbsm.addKeyboardShortcut("Warning",new KBShortcut<ActionEvent>(btnB, KeyCombination.valueOf("ctrl+b"),this::btnBClick));
+        kbsm.addKeyboardShortcut("Error",new KBShortcut<ActionEvent>(btnC, KeyCombination.valueOf("ctrl+c"),this::btnCClick));
     }
 }
