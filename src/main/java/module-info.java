f27f88ef395da1422882db89cac925ca8cf6ef6a
Ossama Nasser <ossnass@gmail.com>
2020-09-19 13:36:21 +0300

    Java 8 support drop
    No longer java 8 support, will only support Java 11+

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module simplefx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires io.github.classgraph;
+    exports io.github.ossnass.fx;
+    exports io.github.ossnass.fx.exceptions;
+    exports io.github.ossnass.fx.keyboard;
+    exports io.github.ossnass.fx.settings;
+}
